120a01a8bb38c1cddfc8ec90f56f7ecbe40c4e1e
Alperen Kitapçı <alperenmirac@gmail.com>
2021-05-25 20:18:36 +0300

    Estetik değişimler/düzeltmeler

--- Posts/urls.py
+++ Posts/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path('flushcomments/<int:post_id>', views.flush_comments, name='flush_comments'),
     path('togglethread/', toggle_thread, name='togglethread'),
     path('deletecomment/<int:comment_id>',views.delete_comment,name='delete_comment'),
-    path('kulllanici/<int:comment_id>',views.show_profile, name='show_profile'),
+    path('kullanici/<int:comment_id>',views.show_profile, name='show_profile'),
 ]
